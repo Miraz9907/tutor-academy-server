66507c483b0ebc9d2dc9a02f4a386b7024ec8483
Miraz9907 <meraz.rahman99@gmail.com>
2022-10-25 22:42:08 +0600

    create courses api

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-
 const courses = require('./data/courses.json');
+const courseDetails = require('./data/course_details.json');
 
 app.get('/', (req, res)=>{
     res.send(' API Running');
@@ -14,6 +14,19 @@ app.get('/', (req, res)=>{
 
 app.get('/courses', (req, res) => {
     res.send(courses)
+})
+
+app.get('/course/:id', (req, res) =>{
+    const id = req.params.id;
+    const courseId = courseDetails.find(course => course.course_id === id);
+    res.send(courseId);
+})
+
+app.get('/courses/:id', (req, res) =>{
+    const id = req.params.id;
+    const selectedCourse = courseDetails.find(course => course.id === id);
+    res.send(selectedCourse);
+    
 })
 
 app.listen(port, ()=>{
