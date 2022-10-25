9a1b59e3dd5ecb171542d78743238b67afeb4751
Miraz9907 <meraz.rahman99@gmail.com>
2022-10-25 21:25:59 +0600

    cors install and setup

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express();
-// const cors = require('cors');
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
-// app.use(cors());
+app.use(cors());
 
 
 const courses = require('./data/courses.json');
